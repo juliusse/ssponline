08675ca3495c0ded04d3c54a8c1fcaa0ebe60f38
Julius Seltenheim <julius.seltenheim@clariness.com>
2021-04-12 13:35:50 +0200

    initial backend setup

---
+++ backend/src/main/java/info/seltenheim/ssponline/SsponlineBackendApplication.java
@@ -0,0 +1,13 @@
+package info.seltenheim.ssponline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsponlineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsponlineBackendApplication.class, args);
+	}
+
+}
