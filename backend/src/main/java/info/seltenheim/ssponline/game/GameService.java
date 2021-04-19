dd45a94b79d66caca560fbcfcbd28d3bd8077cae
Julius Seltenheim <julius.seltenheim@gmail.com>
2021-04-19 16:06:46 +0200

    remove unused code

--- backend/src/main/java/info/seltenheim/ssponline/game/GameService.java
+++ backend/src/main/java/info/seltenheim/ssponline/game/GameService.java
@@ -233,6 +233,8 @@ public class GameService {
         if (losingUnit == UnitType.FLAG) {
             finishGame(gameId, fightAction.getActionId(), winningTeam);
         }
+
+
     }
 
     private void finishGame(String gameId, long lastActionId, Team winningTeam) {
--- backend/src/main/java/info/seltenheim/ssponline/gamestate/GameStateService.java
+++ backend/src/main/java/info/seltenheim/ssponline/gamestate/GameStateService.java
@@ -1,10 +1,8 @@
 package info.seltenheim.ssponline.gamestate;
 
 import info.seltenheim.ssponline.game.model.*;
-import info.seltenheim.ssponline.gamestate.model.Fight;
 import info.seltenheim.ssponline.gamestate.model.GameState;
 import info.seltenheim.ssponline.gamestate.model.Unit;
-import info.seltenheim.ssponline.gamestate.repository.FightRepository;
 import info.seltenheim.ssponline.gamestate.repository.GameStateRepository;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -20,11 +18,10 @@ import java.util.Optional;
 @Transactional
 public class GameStateService {
     private final GameStateRepository gameRepository;
-    private final FightRepository fightRepository;
     private final UnitService unitService;
 
     public GameState processAction(GameAction action) {
-        GameState gameState = null;
+        GameState gameState;
         switch (action.getActionType()) {
             case CONFIGURE:
                 gameState = processConfigure((GameActionConfigure) action);
@@ -116,30 +113,6 @@ public class GameStateService {
                 break;
         }
         return gameRepository.findById(gameId).orElseThrow();
-//        final var unitFrom = unitService.findUnitInLocation(gameId, from)
-//                .orElseThrow();
-//        final var unitToOptional = unitService.findUnitInLocation(gameId, to);
-
-
-        // normal move
-//        if (unitToOptional.isEmpty()) {
-//        unitService.updateUnitPosition(unitFrom.getId(), to);
-//            toggleTeamsTurn(gameState);
-//        return gameState;
-//        }
-
-//        // fight state
-//
-//        final var unitTo = unitToOptional.get();
-//        final var fightResult = unitService.fightAfterMove(unitFrom, unitTo);
-//
-//        if (fightResult != UnitService.FightResult.TIE) {
-//            toggleTeamsTurn(game);
-//        } else {
-//            enterTieMode(game, to);
-//        }
-//
-//        return game;
     }
 
     private GameState processFight(GameActionFight action) {
@@ -160,85 +133,5 @@ public class GameStateService {
 
     public Optional<Unit> findUnitInLocation(@NonNull String gameId, @NonNull Point location) {
         return unitService.findUnitInLocation(gameId, location);
-    }
-
-
-    public GameState moveUnit(@NonNull String gameId, @NonNull Point from, @NonNull Point to) {
-        final var game = gameRepository.findById(gameId)
-                .orElseThrow();
-        final var unitFrom = unitService.findUnitInLocation(gameId, from)
-                .orElseThrow();
-        final var unitToOptional = unitService.findUnitInLocation(gameId, to);
-
-        if (unitToOptional.isEmpty()) {
-            unitService.updateUnitPosition(unitFrom.getId(), to);
-            toggleTeamsTurn(game);
-            return game;
-        }
-
-        final var unitTo = unitToOptional.get();
-        final var fightResult = unitService.fightAfterMove(unitFrom, unitTo);
-
-        if (fightResult != UnitService.FightResult.TIE) {
-            toggleTeamsTurn(game);
-        } else {
-            enterTieMode(game, to);
-        }
-
-        return game;
-    }
-
-    public void chooseUnitForFight(@NonNull String gameId, @NonNull Team team, @NonNull UnitType type) {
-        final var game = gameRepository.findById(gameId).orElseThrow();
-        final var fight = fightRepository.findById(gameId).orElseThrow();
-        final var choice = fight.getChoiceForTeam(team);
-        if (choice != null) {
-            return;
-        }
-
-        fight.setChoiceForTeam(team, type);
-        if (fight.isBothUnitsSet()) {
-            final var fightResult = unitService.fight(fight.getRedChoice(), fight.getBlueChoice());
-
-            switch (fightResult) {
-                case ATTACKER_WINS:
-                    unitService.creatNewUnit(gameId, Team.RED, fight.getRedChoice(), fight.getLocation(), true);
-                    fightRepository.delete(fight);
-                    game.setGameState(info.seltenheim.ssponline.game.model.GameState.TURN);
-                    final var nextTeam = game.getActiveTeam() == Team.RED ? Team.BLUE : Team.RED;
-                    game.setActiveTeam(nextTeam);
-                    gameRepository.save(game);
-                    break;
-                case DEFENDER_WINS:
-                    unitService.creatNewUnit(gameId, Team.BLUE, fight.getBlueChoice(), fight.getLocation(), true);
-                    fightRepository.delete(fight);
-                    game.setGameState(info.seltenheim.ssponline.game.model.GameState.TURN);
-                    final var nextTeam2 = game.getActiveTeam() == Team.RED ? Team.BLUE : Team.RED;
-                    game.setActiveTeam(nextTeam2);
-                    gameRepository.save(game);
-                    break;
-                case TIE:
-                    fight.setRedChoice(null);
-                    fight.setBlueChoice(null);
-                    fightRepository.save(fight);
-                    break;
-            }
-        }
-    }
-
-    public Fight getFightForGame(String gameId) {
-        return fightRepository.findById(gameId).orElseThrow();
-    }
-
-    private void toggleTeamsTurn(GameState game) {
-        final var nextTeam = game.getActiveTeam() == Team.RED ? Team.BLUE : Team.RED;
-        game.setActiveTeam(nextTeam);
-    }
-
-    private void enterTieMode(GameState game, Point location) {
-        game.setGameState(info.seltenheim.ssponline.game.model.GameState.FIGHT);
-        fightRepository.save(new Fight(game.getId(), location));
-
-        gameRepository.save(game);
     }
 }
